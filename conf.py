bbc8b010f6af1b6c068351fb8d675a1fc1f12e0d
cangyeone <305601470@qq.com>
2023-08-23 11:41:17 +0800

    files

---
+++ conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'seisaitools'
+copyright = '2023, yuziye'
+author = 'yuziye'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+#extensions = []
+extensions = [
+     'recommonmark',
+     'sphinx_markdown_tables', 
+     #'myst_parser'
+ ]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+#html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+source_suffix = ['.rst', '.md']
